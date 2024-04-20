8a76d35310174adc9f067c75787935e6ee9b1fa2
abdelkarimain0 <abdelkarimain0@gmail.com>
2024-04-20 12:15:02 +0100

    creating auth api and authCntroller methods

---
+++ app/Http/Controllers/Api/AuthController.php
@@ -0,0 +1,118 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use App\Models\User;
+use Validator;
+
+class AuthController extends Controller
+{
+    /**
+     * Create user
+     *
+     * @param  [string] name
+     * @param  [string] email
+     * @param  [string] password
+     * @param  [string] password_confirmation
+     * @return [string] message
+     */
+    public function register(Request $request)
+    {
+        $request->validate([
+            'name' => 'required|string',
+            'username' => 'required|string|unique:users',
+            'email' => 'required|string|email|unique:users',
+            'password' => 'required|string|min:8',
+            'password_confirmation' => 'required|same:password'
+        ]);
+
+        $user = new User([
+            'name' => $request->name,
+            'username' => $request->username,
+            'email' => $request->email,
+            'password' => bcrypt($request->password),
+        ]);
+
+        if ($user->save()) {
+            $tokenResult = $user->createToken('access-token');
+            $token = $tokenResult->plainTextToken;
+
+            return response()->json([
+                'message' => 'Successfully created user!',
+                'accessToken' => $token,
+            ], 201);
+        } else {
+            return response()->json(['error' => 'Provide proper details']);
+        }
+    }
+
+
+    /**
+     * Login user and create token
+     *
+     * @param  [string] email
+     * @param  [string] password
+     * @param  [boolean] remember_me
+     */
+
+    public function login(Request $request)
+    {
+        $request->validate([
+            'email' => 'required|string|email',
+            'password' => 'required|string'
+        ]);
+
+        $credentials = request(['email', 'password']);
+        if (!Auth::attempt($credentials)) {
+            return response()->json([
+                'message' => 'Unauthorized'
+            ], 401);
+        }
+
+        $user = $request->user();
+        $tokenResult = $user->createToken('access-token');
+        $token = $tokenResult->plainTextToken;
+
+        return response()->json([
+            'message' => 'Successfully logged in',
+            'accessToken' => $token,
+            'token_type' => 'Bearer',
+        ]);
+    }
+
+
+
+    /**
+     * Get the authenticated User
+     *
+     * @return [json] user object
+     */
+    public function user(Request $request)
+    {
+        return response()->json($request->user());
+    }
+
+
+    /**
+     * Logout user (Revoke the token)
+     *
+     * @return [string] message
+     */
+    public function logout(Request $request)
+    {
+        try {
+            $request->user()->tokens()->delete();
+            return response()->json([
+                'message' => 'Successfully logged out'
+            ]);
+        } catch (\Exception $e) {
+            return response()->json([
+                'message' => 'Something went wrong'
+            ], 500);
+        }
+
+    }
+}
