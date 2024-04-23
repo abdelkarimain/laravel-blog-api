f4604f6d4c951401641c894fd8525677ffc60fff
abdelkarimain0 <abdelkarimain0@gmail.com>
2024-04-23 14:08:59 +0100

    -

--- routes/api.php
+++ routes/api.php
@@ -15,6 +15,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+
+
 Route::group(['prefix' => 'auth'], function () {
     Route::post('login', [AuthController::class, 'login']);
     Route::post('register', [AuthController::class, 'register']);
